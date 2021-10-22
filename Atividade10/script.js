daf1b999905da87837c2c74544ef30858e1c6cba
IgorRafael1020 <49539371+IgorRafael1020@users.noreply.github.com>
2021-10-22 01:50:51 -0300

    Add files via upload

---
+++ Atividade10/script.js
@@ -0,0 +1,18 @@
+var aluno1 = new Object(); 
+aluno1.RA = 123456;
+aluno1.Nome = "Robson";
+
+alert("Objeto aluno1 \nRA: " + aluno1.RA + "\nNome: " + aluno1.Nome);
+
+var aluno2 = {}; 
+aluno2.RA = 456123;
+aluno2.Nome = "Patrícia";
+
+alert("Objeto aluno2 \nRA: " + aluno2.RA + "\nNome: " + aluno2.Nome);
+
+var aluno3 = {
+	RA : 456987,
+	Nome : "Kleber"
+}
+
+alert("Objeto aluno3 \nRA: " + aluno3.RA + "\nNome: " + aluno3.Nome);
