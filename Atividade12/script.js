575a9264bdea6bc16160f097673cd0b869e43226
IgorRafael1020 <49539371+IgorRafael1020@users.noreply.github.com>
2021-11-07 20:19:30 -0300

    Add files via upload

---
+++ Atividade12/script.js
@@ -0,0 +1,11 @@
+function quebrar(obj) {
+	obj.style.backgroundImage = "url('janelaquebra.png')";
+}
+
+function abrir(obj) {
+	obj.style.backgroundImage = "url('janelaaberta.png')";
+}
+
+function fechar(obj) {
+	obj.style.backgroundImage = "url('janelafechada.png')";
+}
